1f8fedb09315355c6bd8271a863cdd5035a57c8c
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-01-22 20:16:57 -0800

    MUCH CLEARER layer definitions

--- res_net.py
+++ res_net.py
@@ -14,20 +14,21 @@ class ResNet(nn.Module):
         # TODO: figure out how to pass these explicitly.  Also this needs to be renamed.
         internal_channels = 64
         # expand "color" channels to "the main channel"
-        self.conv1      = self._option(1, layers, nn.Conv2d(input_channels, internal_channels, kernel_size=7, stride=2, padding=3))
+        self.conv1      = self._option(1, layers, nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3))
         self.batchnorm1 = self._option(2, layers, nn.BatchNorm2d(internal_channels))
         self.relu       = self._option(3, layers, nn.ReLU())
         self.maxpool    = self._option(4, layers, nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+
         ## ResNet blocklayers
-        self.blocklayer1 = self._option(5, layers, self._make_blocklayer(block_counts[0], input_channels=64, internal_channels=64, stride=1))
-        self.blocklayer2 = self._option(6, layers, self._make_blocklayer(block_counts[1], input_channels=256, internal_channels=128, stride=2))
-        self.blocklayer3 = self._option(7, layers, self._make_blocklayer(block_counts[2], input_channels=512, internal_channels=256, stride=2))
-        self.blocklayer4 = self._option(8, layers, self._make_blocklayer(block_counts[3], input_channels=1024, internal_channels=512, stride=2))
+        self.blocklayer1 = self._option(5, layers, self._make_blocklayer(block_counts[0], 64, 256))
+        self.blocklayer2 = self._option(6, layers, self._make_blocklayer(block_counts[1], 256, 512, stride=2))
+        self.blocklayer3 = self._option(7, layers, self._make_blocklayer(block_counts[2], 512, 1024, stride=2))
+        self.blocklayer4 = self._option(8, layers, self._make_blocklayer(block_counts[3], 1024, 2048, stride=2))
 
         ## cleanup and postprocessing
         self.avgpool =        self._option(9, layers, nn.AdaptiveAvgPool2d((1, 1)))
         self.reshape =        self._option(10, layers, self._final_reshape)
-        self.fullyconnected = self._option(11, layers, nn.Linear(512 * EXPANSION_FACTOR, num_classes))
+        self.fullyconnected = self._option(11, layers, nn.Linear(2048, num_classes))
 
     def forward(self, input):
         ## apply the pretreatment layers, generating activations.
@@ -46,16 +47,13 @@ class ResNet(nn.Module):
         activation = self.fullyconnected(activation)
         return activation
 
-    def _make_blocklayer(self, block_count, input_channels, internal_channels, stride):
-        downsampler = None
-
-        # were going to be building up a layers list.
+    def _make_blocklayer(self, block_count, input_channels, output_channels, stride = 1):
+        # build up a layers list
         layers = []
-
-        # note that ResNetBlock will return size input_channels * 4!!  this is super confusing!
-        layers.append(ResNetBlock(input_channels, internal_channels, stride))
+        # first layer reconciles the size difference between the input and the output.
+        layers.append(ResNetBlock(input_channels, output_channels, stride))
         for i in range(block_count - 1):
-            layers.append(ResNetBlock(internal_channels * EXPANSION_FACTOR, internal_channels))
+            layers.append(ResNetBlock(output_channels, output_channels))
 
         # splat the layers list.
         return nn.Sequential(*layers)
